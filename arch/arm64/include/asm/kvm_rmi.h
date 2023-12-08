fd3ba146ee9fcd3df0c5fe3e0f1f7f45ac05b7ec
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-12-08 09:04:29 +0000

    KVM: arm64: CCA: Propagate max SVE vector length from the RMM
    RMM provides the maximum vector length it supports for a guest in its
    feature register. Make it visible to the rest of KVM and to userspace
    via KVM_REG_ARM64_SVE_VLS.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- arch/arm64/include/asm/kvm_rmi.h
+++ arch/arm64/include/asm/kvm_rmi.h
@@ -95,6 +95,7 @@ struct realm_rec {
 
 void kvm_init_rmi(void);
 u32 kvm_rmm_ipa_limit(void);
+unsigned int kvm_realm_sve_max_vl(void);
 
 u64 kvm_realm_reset_id_aa64dfr0_el1(const struct kvm_vcpu *vcpu, u64 val);
 
